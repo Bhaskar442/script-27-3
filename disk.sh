6288d4ba0d01ca866a7f83e719e5c2f1860c1000
Ubuntu <ubuntu@ip-172-31-29-230.ec2.internal>
2026-03-27 04:28:58 +0000

    disk script added

---
+++ disk.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+THRESHOLD=70
+HOSTNAME=$(hostname)
+DATE=$(date '+%Y-%m-%d_%H:%M:%S')
+LOGFILE="/var/log/disk.log"
+
+USAGE=$(df -h / | awk 'NR==2 {print$5}' | sed 's/%//')
+
+if [ "$THRESHOLD" -gt "$USAGE" ]; then
+	echo "$HOSTNAME | $DATE | warning: disk usage is more $USAGE" >> $LOGFILE
+else
+	echo "$HOSTANEM | $DATE | info: disk usage is normal $USAGE" >> $LOGFILE
+fi
+
